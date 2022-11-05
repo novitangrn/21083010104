3b50f558a3e3a2bdd8ef0e4684d0d7e77f2a7f13
Novita Anggraini <90380785+novitangrn@users.noreply.github.com>
2022-11-05 19:55:20 +0700

    Sisop Week 2

--- array.sh
+++ array.sh
@@ -4,8 +4,8 @@
 distroLinux=("Mint" "Ubuntu" "Kali" "Arch" "Debian" "CentOS" "Fedora")
 
 # random distro
-let pilih=$RANDOM%7
+let pick=$RANDOM%7
 
 # eksekusi
-echo "Saya Memilih Distro $pilih, ${distroLinux[$pilih]}!"
+echo "Saya Memilih Distro $pick, ${distroLinux[$pick]}!"
 
